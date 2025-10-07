5c42dcac16759994964a1c1b18f061547ba9686b
lucasemiriam2017-jpg <lucasemiriam2017@gmail.com>
2025-10-07 17:03:33 -0300

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
-class Config:
-    # URL do banco, pode vir do Render ou do .env local
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://usuario:senha@localhost:5432/meubanco")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "uploads"
+# Caminhos padrão
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+CSV_FILE = os.path.join(BASE_DIR, "cadastros.csv")
+
+# Cria as pastas se não existirem
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
